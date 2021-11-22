d5184e82d5418b5ec3884bb161fea01d865506f3
sara russert <sararussert@saras-MacBook-Pro-2.local>
2021-11-22 13:49:34 -0800

    adds setup for exam

---
+++ clear.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const Char = require('./charModel');
+
+// write a function that connects to the db
+// uses the `.deleteMany` method on the cat collection with no object properties specified
+// disconnect from the db
+
+async function clear() {
+  mongoose.connect(process.env.DB_URL);
+  try {
+    await Char.deleteMany({});
+    console.log('Characters cleared');
+  } catch (err) {
+    console.error(err)
+  } finally {
+    mongoose.disconnect();
+  }
+}
+
+clear();
+// to clear the database:
+// make sure your server is not running
+// in terminal go to project root
+// type `node clear.js`
